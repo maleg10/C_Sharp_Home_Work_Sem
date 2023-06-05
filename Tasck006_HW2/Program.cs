d305ff7668d5e47c9dc0a5e79a2da80b0c96ef7b
Oleg <maleg34@yandex.ru>
2023-06-06 02:01:42 +0300

    added folder and upd text for Home work_2

--- Tasck006_HW2/Program.cs
+++ Tasck006_HW2/Program.cs
@@ -2,3 +2,28 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра числа: {ThirdDigit(number1)}");
+
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        } 
+        result = number % 10;
+    }
+   return result; 
+}
+ 
