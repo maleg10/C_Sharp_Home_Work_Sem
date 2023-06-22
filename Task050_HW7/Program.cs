4659f1419a2e03a41fcb77a513cc786d39e0a670
Oleg <maleg34@yandex.ru>
2023-06-23 00:50:05 +0300

    added home work lesson_7

--- Task050_HW7/Program.cs
+++ Task050_HW7/Program.cs
@@ -8,5 +8,72 @@
 8 4 2 4
 [1,7] -> такого числа в массиве нет [1,7]-это позиция элемента 1 - строка, 7 - столбец
 */
-//1. Задать  двумерный массив
-//2. 
+
+
+int Prompt (string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int val = 0;
+    while ((int.TryParse(value, out val)) != true)
+    {
+        Console.Write("Неверный ввод. Повторите попытку: ");
+        value = Console.ReadLine();
+    }
+   
+    return val;
+}
+
+
+void PrintMatrix (int[,] array)
+{
+    for (int row = 0 ; row < array.GetLength(0) ; row ++)
+    {
+        for (int colum = 0 ; colum<array.GetLength(1) ; colum ++)
+        {
+            Console.Write($" {array [row , colum]}");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] CreateRandoMatrix (int rows , int colums , int minValue , int maxValue)
+{
+    int [,] matrix = new int[rows,colums];
+    maxValue ++;
+    for (int row = 0 ; row < matrix.GetLength(0) ; row ++)
+    {
+        for (int colum = 0 ; colum<matrix.GetLength(1) ; colum ++)
+        {
+            matrix[row,colum] = new Random().Next(1,100);
+        }
+    }
+    return matrix;
+}
+
+int row = Prompt("Ведите количество строк массива: ");
+int col = Prompt("Введите количество столбцов массива: ");
+
+int[,] matr = CreateRandoMatrix(row,col, 1, 100);
+PrintMatrix(matr);
+
+int temp1 = row;
+int temp2 = col;
+string temp = string.Empty;
+int i = 1;
+while (i > 0)
+{
+    row = Prompt("Введите строку элемента: ");
+    col = Prompt("Введите столбец элемента: ");
+    int a = row-1;
+    int b = col-1;
+    if (row<=0 || row>matr.GetLength(0) || col<=0 || col>matr.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента нет в массиве");
+    }
+    else
+    {
+        Console.WriteLine($"Искомый элемент в массиве равен: {matr[a,b]}");
+    }
+        break;
+}
--- Task052_HW7/Program.cs
+++ Task052_HW7/Program.cs
@@ -6,3 +6,62 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+Console.Write("Введите количество строк (m) массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество стобцов (n) массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0 ; i < matrix.GetLength(0) ; i ++)
+    {
+        for (int j = 0 ; j<matrix.GetLength(1) ; j ++)
+        {
+            Console.Write($" {matrix [i , j]}");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+int[,] RandomMatrix(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 50);
+        }
+    }
+     return result;
+}
+
+int [,] matr = RandomMatrix(m,n,1,50);
+PrintMatrix(matr);
+
+    Console.WriteLine("Среднее арифметическое столбцов:");
+
+    double arithmeticalMean = 0;
+
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            arithmeticalMean += matr[i, j];
+        }
+        arithmeticalMean = Math.Round(arithmeticalMean / m, 2);
+        Console.Write(arithmeticalMean + "\t|");     
+    }
+   
+
+
+
+
+
+
+
+
+
